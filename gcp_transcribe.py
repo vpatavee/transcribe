06efe5b135c71c19d55d3ac8ab1adb924f8de5a6
vpatavee <vpatavee@gmail.com>
2021-07-05 14:33:45 -0700

    add argparse

--- gcp_transcribe.py
+++ gcp_transcribe.py
@@ -1,5 +1,6 @@
 import os
 from io import BytesIO
+import argparse
 
 from google.cloud import speech_v1p1beta1
 from google.cloud.speech_v1p1beta1 import enums, types
@@ -89,4 +90,10 @@ def gcp_transcribe(audio_byte):
 
 
 if __name__ == '__main__':
-    transcribe_from_folder("/Users/pataveemeemeng/Downloads/lydia")
+    parser = argparse.ArgumentParser(description='Transcribe a folder containing mp3 and save their transcriptions'
+                                                 'in the same folder as txt file')
+    parser.add_argument('folder', help='absolute path to folder containing mp3')
+    args = parser.parse_args()
+
+    # "/Users/pataveemeemeng/Downloads/lydia"
+    transcribe_from_folder(args.folder)
